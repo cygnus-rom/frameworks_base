ed9a8b22c6409f9e21828c16ac68c1cc8c63d23d
ezio84 <brabus84@gmail.com>
2018-02-14 00:24:18 +0100

    BatteryMeterView: Show a bolt ⚡when charging
    idoybh:
    * Only when using the text only style
    * Improve some code visually: make it easier to read, use one less var, correctly indent and don't call an inner function var with a name format used for globals
    
    [jhonboy121]: adapt to A13
    
    Change-Id: I27f745e3458fda7c415a75d6e52f183e327cb498
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Dhruvgera <dhruvgera61@gmail.com>

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -272,10 +272,16 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         // Setting text actually triggers a layout pass (because the text view is set to
         // wrap_content width and TextView always relayouts for this). Avoid needless
         // relayout if the text didn't actually change.
-        if (!TextUtils.equals(mBatteryPercentView.getText(), percentText)) {
-            mBatteryPercentView.setText(percentText);
+        if (TextUtils.equals(mBatteryPercentView.getText(), percentText)) {
+            return;
         }
 
+        // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
+        // to load its emoji colored variant with the uFE0E flag
+        // only use it when there is no batt icon showing
+        String indication = mCharging && (mBatteryStyle == BATTERY_STYLE_TEXT)
+                ? "\u26A1\uFE0E " : "";
+        mBatteryPercentView.setText(indication + percentText);
         setContentDescription(
                 getContext().getString(mCharging ? R.string.accessibility_battery_level_charging
                         : R.string.accessibility_battery_level, mLevel));
