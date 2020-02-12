59cab9739298af40ca247e5ce1671cbe9640f482
ezio84 <brabus84@gmail.com>
2020-02-12 19:41:28 +0100

    SystemUI: NetworkTraffic: Don't show two instances in expanded statusbar
    we keep the one on top right of the screen (there we have more space
    and in the future can add more net traffic info)
    
    Change-Id: I098c93b9dd0edd146d08fd23c8c721274b9859ec
    Signed-off-by: Dhruv <dhruvgera61@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1901,6 +1901,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (!isExpanded) {
             mRemoteInputManager.onPanelCollapsed();
         }
+        ((StatusBarIconControllerImpl) mIconController).onPanelExpanded(isExpanded);
     }
 
     public ViewGroup getNotificationScrollLayout() {
@@ -3680,6 +3681,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         updateScrimController();
         mPresenter.updateMediaMetaData(false, mState != StatusBarState.KEYGUARD);
         updateKeyguardState();
+    
+        ((StatusBarIconControllerImpl) mIconController).setKeyguardShowing(mState == StatusBarState.KEYGUARD);
         Trace.endSection();
     }
 
