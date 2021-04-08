P-404/android_frameworks_base

SystemUI: Match QS tiles color with accent color
Previou one was slightly muted, which looked inconsistent with other UI elements.

packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileBaseView.java
@@ -127,7 +127,7 @@ public class QSTileBaseView extends com.android.systemui.plugins.qs.QSTileView {
         mColorDisabled = Utils.getDisabled(context,
                 Utils.getColorAttrDefaultColor(context, android.R.attr.textColorTertiary));
         mColorInactive = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary);
-	owl_404 =  ColorUtils.setAlphaComponent(activeColor, 200);
+	owl_404 =  ColorUtils.setAlphaComponent(activeColor, 255);
         setPadding(0, 0, 0, 0);
         setClipChildren(false);
         setClipToPadding(false);

cygnus-rom/frameworks_base

Signed-off-by: Dhruv <dhruvgera61@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileBaseView.java
@@ -124,7 +124,7 @@ public class QSTileBaseView extends com.android.systemui.plugins.qs.QSTileView {
         mColorDisabled = Utils.getDisabled(context,
                 Utils.getColorAttrDefaultColor(context, android.R.attr.textColorTertiary));
         mColorInactive = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary);
-	cygnus_bar =  ColorUtils.setAlphaComponent(activeColor, 200);
+	cygnus_bar =  ColorUtils.setAlphaComponent(activeColor, 255);
         setPadding(0, 0, 0, 0);
         setClipChildren(false);
         setClipToPadding(false);